eefb3b25eb39f2d8fc307c0a59044443b0130992
Uchencho <aloziekelechi17@gmail.com>
2021-03-12 17:40:22 +0100

    test cases

--- internal/app/app.py
+++ internal/app/app.py
@@ -3,26 +3,30 @@ from internal.client.main import S3Client
 
 class App:
 
+    s3_client = None
+
     function_map = {
         'renewals' : get_file_details
     }
 
-    def __init__(self, appName):
+    def __init__(self, appName, s3_client = None):
         print("Initializing with ", appName)
 
-        new_client = S3Client()
-        print(new_client) # Rather than printing, you would pass this into workflow
+        self.s3_client = s3_client
+        if not s3_client:
+            self.s3_client = S3Client()
+        print(self.s3_client) # Rather than printing, you would pass this into workflow
 
 
     def consume(self, event_key):
         available_keys = self.function_map.keys()
 
-        print("Consuming event with wvent key: ", event_key)
+        print("Consuming event with event key: ", event_key)
 
         for k in available_keys:
             if k in event_key:
                 func_to_invoke = self.function_map[k]
-                func_to_invoke(event_key)
-                return
+                r = func_to_invoke(event_key, self.s3_client)
+                return r
         
         raise KeyError(f"{event_key} is not registered among consumers")
---
+++ internal/app/test_app.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+import pandas as pd
+from internal.app.app import App
+
+
+class MockS3Client:
+
+    def read_prev_data_updates(self, file_read_name):
+
+        data = {'year': [2014, 2018,2020,2017], 
+            'make': ["toyota", "honda","hyndai","nissan"],
+            'model':["corolla", "civic","accent","sentra"]
+        }
+
+        return pd.DataFrame(data)
+
+
+
+class TestRenewals(TestCase):
+
+    expected_number_of_rows = 4
+
+    def test_renewals_returns_expected_number_of_rows(self):
+        a = App("testApp", MockS3Client())
+        result = a.consume('renewals')
+        self.assertEqual(self.expected_number_of_rows, result)
+        
+
+    def test_renewals_raises_error_with_invalid_df(self):
+        
+        with self.assertRaises(EnvironmentError):
+            a = App("testApp")
+            a.consume('renewals')
+
+
